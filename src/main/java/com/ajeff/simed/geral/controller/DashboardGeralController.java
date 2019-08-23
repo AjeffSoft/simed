554792a2bcb1365cf38d705cdf5d7f473ad4f7ea
Jefferson Freitas <ajeffsoft@gmail.com>
2019-08-23 15:08:32 -0300

    Inicio de implementação da Pesquisa Satisfação

--- src/main/java/com/ajeff/simed/geral/controller/DashboardGeralController.java
+++ src/main/java/com/ajeff/simed/geral/controller/DashboardGeralController.java
@@ -30,4 +30,5 @@ public class DashboardGeralController {
 		return mv;
 	}
 	
+	
 }
--- src/main/java/com/ajeff/simed/satisfacao/controller/DashboardSatisfacaoController.java
+++ src/main/java/com/ajeff/simed/satisfacao/controller/DashboardSatisfacaoController.java
@@ -1,18 +1,27 @@
 package com.ajeff.simed.satisfacao.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ajeff.simed.geral.security.UsuarioSistema;
+import com.ajeff.simed.geral.service.UsuarioService;
+
 @Controller
 public class DashboardSatisfacaoController {
 	
+	@Autowired
+	private UsuarioService usuarioService;
+		
 
-	
-	@GetMapping("/satisfacao")
-	public ModelAndView dashboard() {	
-		ModelAndView mv = new ModelAndView("Satisfacao/Dashboard");
+	@GetMapping("/pesqsatisfacao")
+	public ModelAndView inicio(@AuthenticationPrincipal UsuarioSistema usuarioSistema) {	
+		ModelAndView mv = new ModelAndView("Satisfacao/DashboardSatisfacao");
+		mv.addObject("empresas", usuarioService.buscarEmpresaPorUsuario(usuarioSistema.getUsuario().getId()));
 		return mv;
 	}
+	
 	
 }
